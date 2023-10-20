0600e0c402afd351bd9836aff87b7c1632793522
arthur <artsoares2006@gmail.com>
2023-10-19 22:32:57 -0300

    aula 1 js

---
+++ java/script.js
@@ -0,0 +1,27 @@
+//let pessoa1 = 'arthur';
+//var pessoa2 = 'joao';
+//const pessoa3 = 'lana';
+//variaveis
+
+//let mensagem = 'Ola essa e a mensagem da minh variavel';
+//console.log(mensagem);
+//mensagem = 'essa e a nova mensagem';
+//console.log(mensagem);
+
+//bool: booleano: true or false
+
+//let num1 = 15;
+//let num2 = 20;
+
+//let soma = num1 + num2;
+//console.log(soma);
+
+//let multi = num1 * num2;
+//console.log(multi);
+
+//let div = num1 / num2;
+//console.log(div);
+
+//let sub = num1 - num2;
+//console.log(sub);
+
